3b41d593aa60fd621c2fcc3d433d660da2b33acb
LizzyYang <lizzyyang@allpyra.com>
2019-07-19 14:39:00 +0800

    环境搭建

---
+++ src/java/com.atguigu.java/ApplicationSharding.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C), 2015-2019, 深圳市启明星电子商务有限公司
+ *
+ * @Title: ApplicationSharding
+ * @Package com.atguigu.java
+ * @author: apple
+ * @date: 2019-07-19 00:14
+ * @since JDK 1.8
+ */
+package com.atguigu.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName : ApplicationSharding
+ * @AUTHOR :  apple
+ * @DATE :    2019-07-19 00:14
+ * @DESCRIPTION : TODO(用一句话描述该类做什么)
+ * @since JDK 1.8
+ */
+@SpringBootApplication
+public class ApplicationSharding {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationSharding.class, args);
+    }
+}
